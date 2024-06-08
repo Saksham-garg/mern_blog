992a928b98e1f0f2ec5f5eb7f9772b92d0246148
Saksham-garg <sakshamgarg142@hotmail.com>
2024-06-08 20:59:42 +0530

    created header section

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Header } from './components/index.js'
 import { createBrowserRouter,RouterProvider } from "react-router-dom"
 import routes from "@/routes/routes.jsx"
 
 const routesPath = createBrowserRouter(routes)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={routesPath}></RouterProvider>
+      <RouterProvider router={routesPath}>
+      </RouterProvider>
   </React.StrictMode>,
 )
--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,28 +1,35 @@
 import { Dashboard, Home, Projects, SignIn, SignUp, About } from "@/views";
+import { Header } from "../components";
 
 export default [
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/sign-in",
-    element: <SignIn />,
-  },
-  {
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "/projects",
-    element: <Projects />,
-  },
+    path:'/',
+    element:<Header />,
+    children:[
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+    ]
+  }
 ];
