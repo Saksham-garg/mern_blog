06cc34eaf61be4f05c27ce3cd38a371a9e9b8976
Saksham-garg <sakshamgarg142@hotmail.com>
2024-06-11 14:53:14 +0530

    feat: setup redux store for user

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import { Header } from './components/index.js'
 import { createBrowserRouter,RouterProvider } from "react-router-dom"
 import routes from "@/routes/routes.jsx"
+import { store } from './stores/stores.js'
+import { Provider } from 'react-redux'
 
 const routesPath = createBrowserRouter(routes)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+     <Provider store={store}>
       <RouterProvider router={routesPath}>
       </RouterProvider>
-  </React.StrictMode>,
+     </Provider>
 )
---
+++ client/src/stores/stores.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userSlice from './user/userSlice.js'
+export const store = configureStore({
+  reducer: {
+    user:userSlice
+  },
+})
