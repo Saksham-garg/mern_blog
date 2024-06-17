0260c690fedc641e8dad58c659203ca7aee2e8fb
Saksham-garg <sakshamgarg142@hotmail.com>
2024-06-17 13:48:49 +0530

    Add pre installation

--- api/app.js
+++ api/app.js
@@ -11,12 +11,20 @@ import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 import postRoutes from './routes/post.routes.js'
 import commentRoutes from './routes/comment.routes.js'
+import path from 'path'
+
+const __dirname = path.resolve()
 
 app.use('/api/v1/auth/',authRoutes)
 app.use('/api/v1/user/',userRoutes)
 app.use('/api/v1/post/',postRoutes)
 app.use('/api/v1/comment/',commentRoutes)
 
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.use("*", (req,res,next) => {
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 // Global Error middleware
 app.use((err,req,res,next) => {
